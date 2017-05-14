40328f433ebcfb99f924f6e64681a5249ce94672
morskee <kammoren@icloud.com>
2017-05-14 16:01:57 +0200

    comments

--- Form1.cs
+++ Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Speech.Recognition;
+using System.Speech.Recognition; //w odwołaniach dodane zostało System.Speech to poprawnego działania
 using System.Speech.Synthesis;using System.Threading;
 
 
@@ -22,7 +22,7 @@ namespace HMI_project
 
         private void Form1_Load_1(object sender, EventArgs e)
         {
-            //RecognizeSpeech();
+            RecognizeSpeech();
 
             label12.Text = DateTime.Now.ToLongTimeString();
 
@@ -32,27 +32,27 @@ namespace HMI_project
 
         }
 
-        private void trackBar1_Scroll(object sender, EventArgs e)
+        private void trackBar1_Scroll(object sender, EventArgs e) //Scroll czyli co bedzie sie dziac jesli sie bedzie ruszalo trackBarem ;)
         {
-            aGauge1.Value = trackBar1.Value;
+            aGauge1.Value = trackBar1.Value; //wartosc zegara aGauge1 jest równa wartosci z trackbara po to aby trackbar zmienial wartosc na zegarze
 
             label11.Text = "";
-            if (trackBar2.Value > 0 && trackBar1.Value > 0)
+            if (trackBar2.Value > 0 && trackBar1.Value > 0) //petla if jesli spelnione sa dwa waurnki 
             {
-                float zmienna = trackBar2.Value + trackBar1.Value;
-                float zmienna2 = zmienna / 25;
+                float zmienna = trackBar2.Value + trackBar1.Value; //zmienna - dodanie wartosci z obrotomierza i predkosciomierza
+                float zmienna2 = zmienna / 25; //zmienna2 - to wartosc spalania na L/100KM przez 25 bo tak dobrze liczby wychodzily 
 
-                label11.Text = "" + zmienna2.ToString("F2") + "L/100KM";
+                label11.Text = "" + zmienna2.ToString("F2") + "L/100KM"; //wpisanie do labela wartosci "zmienna2"; ToString("F2") oznacza pokazywanie się 2 miejsc po przecinku w "zmienna2"
             }
         }
 
-        private void timer1_Tick(object sender, EventArgs e)
+        private void timer1_Tick(object sender, EventArgs e) //timer1_Tick co ma się dziać za każdym ticknięciem timera 
         {
-            label12.Text = DateTime.Now.ToLongTimeString();
+            label12.Text = DateTime.Now.ToLongTimeString(); //pobranie godziny z systemu
             
-            label13.Text = DateTime.Now.ToLongDateString();
+            label13.Text = DateTime.Now.ToLongDateString(); //pobranie daty z systemu
 
-            timer1.Start();
+            timer1.Start(); //wlaczenie timera
         }
 
         private void trackBar2_Scroll(object sender, EventArgs e)
@@ -86,12 +86,13 @@ namespace HMI_project
             float zmienna = trackBar2.Value + trackBar1.Value;
             float zmienna2 = zmienna / 25;
             float zmienna_temp = trackBar3.Value;
-            chart1.Series["Speed"].Points.AddXY(0, zmienna2);
-            chart1.Series["Temp"].Points.AddXY(0, zmienna_temp);
+            chart1.Series["Speed"].Points.AddXY(0, zmienna2); //seria Speed na wykresie X to 0 ale zmienia się to za kazdym tickinieciem timera2
+            chart1.Series["Temp"].Points.AddXY(0, zmienna_temp); //seria Temp również posiada X równy 0 
             timer2.Start();
         }
-        
-        /*static SpeechRecognitionEngine _recognizer = null;
+        //kod pobrany z Lab3_Instrukcja.pdf
+        //kazde kolejne komendy dodane zostaly metoda kopiuj/wklej
+        static SpeechRecognitionEngine _recognizer = null;
         SpeechSynthesizer speechSynthesizer = new SpeechSynthesizer();
         void RecognizeSpeech()
         {
@@ -150,15 +151,15 @@ namespace HMI_project
         {
             this.listBox1.Items.Add(">SpeechRecognitionEngine: Unrecognized command...");
         }
-        */
-        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        
+        private void checkBox1_CheckedChanged(object sender, EventArgs e) 
         {
-            if (checkBox1.Checked)
+            if (checkBox1.Checked) //jesli checkBox1 jest zaznaczony
             {
-                this.pictureBox16.Load("temp.jpg");
+                this.pictureBox16.Load("temp.jpg"); //jesli zaznaczony laduje obrazek ten
             }
             else
-                this.pictureBox16.Load("nic.jpg");
+                this.pictureBox16.Load("nic.jpg"); //jesli nie zaznaczony to ten
 
         }
 
@@ -191,9 +192,5 @@ namespace HMI_project
             else
                 this.pictureBox17.Load("esp.jpg");
         }
-
-        //TODO: zablokowanie, odpalanie silnika
-        //TODO: checkboxy
-
     }
 }
